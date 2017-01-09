b77c8d56c935e4af115d14f67b300db0fc16ab81
Victor Castrejon <vcastrejont@gmail.com>
2017-01-09 10:22:07 -0700

    removed favicon dependency

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path      = require('path');
-var favicon   = require('serve-favicon');
 var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
